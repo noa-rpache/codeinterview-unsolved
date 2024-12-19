5358ddae1ec67887f785f3a6bad8fb2904b145aa
noa-sb <noa.rpache@udc.es>
2024-12-19 11:57:16 +0100

    test added

--- src/test/java/es/merlinsoftware/SortingScoresTest.java
+++ src/test/java/es/merlinsoftware/SortingScoresTest.java
@@ -1,14 +1,11 @@
 package es.merlinsoftware;
 
-import static org.junit.Assert.assertTrue;
-
 import es.merlinsoftware.pojo.ProductSales;
 import es.merlinsoftware.pojo.ProductStock;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class SortingScoresTest {
@@ -31,6 +28,39 @@ public class SortingScoresTest {
         Long[] expectedResult = {2L, 4L, 3L, 1L};
 
         Assert.assertArrayEquals(Solution.sortProductsByScores(50, 50, productStock, productSales).toArray(),
+                expectedResult);
+    }
+
+    @Test
+    public void testHappyPathWithScores() {
+        List<ProductSales> productSales = new ArrayList<>();
+        productSales.add(new ProductSales(1L, 10000.0));
+        productSales.add(new ProductSales(2L, 50000.0));
+        productSales.add(new ProductSales(3L, 100000.0));
+        productSales.add(new ProductSales(4L, 75000.0));
+
+
+        List<ProductStock> productStock = new ArrayList<>();
+        productStock.add(new ProductStock(1L, 100000L));
+        productStock.add(new ProductStock(2L, 400000L));
+        productStock.add(new ProductStock(3L, 200000L));
+        productStock.add(new ProductStock(4L, 300000L));
+
+
+        Long[] expectedResult = {2L, 4L, 3L, 1L};
+
+        Assert.assertArrayEquals(Solution.sortProductsByScores(10, 90, productStock, productSales).toArray(),
+                expectedResult);
+    }
+
+    @Test
+    public void testEmptyList() {
+        List<ProductSales> productSales = new ArrayList<>();
+        List<ProductStock> productStock = new ArrayList<>();
+
+        Long[] expectedResult = {};
+
+        Assert.assertArrayEquals(Solution.sortProductsByScores(10, 90, productStock, productSales).toArray(),
                 expectedResult);
     }
 }
